8943843da64769a8703c2aed8fcf8f1ef87c3781
jahanzaib2630 <jahanzaib@clustox.com>
2023-03-23 12:14:51 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Inventory from './Pages/Maininventory/Inventory';
+import BobsCollection from './Pages/Bobs_Collection/BobsCollection';
+import { BrowserRouter, Route, Routes, Navigate   } from "react-router-dom";
+import Home from './Pages/Home/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <BrowserRouter>
+       <Routes>
+         
+       {/* <Route path="/login">
+          <Login />
+        </Route> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign_up" element={<Register />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/maininventory" element={<Inventory />} />
+          <Route path="/bobs_collection" element={<BobsCollection />} />
+          <Route path="/home" element={<Home />} />
+       </Routes>
+      </BrowserRouter>
+   </>
   );
 }
 
