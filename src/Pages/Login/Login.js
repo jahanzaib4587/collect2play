a82afa4cd9e0c125735ac1996a202bb657deac6c
jahanzaib2630 <jahanzaib@clustox.com>
2023-03-27 12:22:34 +0500

    Form added, rules added to Form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,34 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
-import { Input, Alert } from "antd";
+import { Input, Alert, Form } from "antd";
 import { Switch } from "antd";
 import { apiCall } from "../../Api/ApiCall";
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import './Login.css';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithRedirect,
+  getRedirectResult,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import "./Login.css";
 import { motion } from "framer-motion";
-
 
 const Login = () => {
   const navigate = useNavigate();
   const [hideshowemail, setHideshowEmail] = useState(true);
   const [hideshowphone, setHideshowPhone] = useState(false);
-  const [showNotification, setShowNotification] = useState(false)
-  const [notificationText, setNotificationText] = useState('abc')
+  const [showNotification, setShowNotification] = useState(false);
+  const [notificationText, setNotificationText] = useState("abc");
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [notificationType, setNotificationType] = useState('success')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [notificationType, setNotificationType] = useState("success");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const firebase_relay_id = "ifV6F0TgYgXDqybVfJylNYHzwdD2";
-  const [isSignup, setIsSignup] = useState(false)
+  const [isSignup, setIsSignup] = useState(true);
   const [OTP, setOTP] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState();
 
+  const [form] = Form.useForm();
 
   function otpfield() {
-    setOTP(!OTP)
+    setOTP(!OTP);
   }
   const firebaseConfig = {
     apiKey: "AIzaSyBtZcEG4vcJHBa3UlDEtPpTJ3D5TNpkzAs",
@@ -38,7 +46,7 @@ const Login = () => {
     storageBucket: "collect2play-5c3d4.appspot.com",
     messagingSenderId: "432235957600",
     appId: "1:432235957600:web:f3c383932428a00cec2205",
-    measurementId: "G-WY68V7QXMR"
+    measurementId: "G-WY68V7QXMR",
   };
   const app = initializeApp(firebaseConfig);
 
@@ -49,15 +57,14 @@ const Login = () => {
     if (showNotification) {
       setTimeout(() => {
         setShowNotification(false);
-        setNotificationText('')
+        setNotificationText("");
       }, 3000);
     }
-    return () => {
-    };
+    return () => {};
   }, [showNotification]);
 
   const handleGoogleSignIn = async () => {
-
+    debugger;
     signInWithRedirect(auth, provider)
       .then(() => {
         return getRedirectResult(auth);
@@ -66,6 +73,7 @@ const Login = () => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
+
         localStorage.setItem("access_token", token);
 
         // The signed-in user info.
@@ -74,57 +82,64 @@ const Login = () => {
         navigate("/");
       })
       .catch((error) => {
-        setNotificationText(error.message)
-        setShowNotification(true)
-        setNotificationType('error')
+        setNotificationText(error.message);
+        setShowNotification(true);
+        setNotificationType("error");
         // Handle errors here.
         console.log(error);
       });
   };
 
   const handleSignIn = (event) => {
-    debugger
-    event.preventDefault();
-    setIsLoading(true);
+    form
+      .validateFields()
+      .then(() => {
+        setIsLoading(true);
 
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        localStorage.setItem("access_token", user.accessToken);
-        console.log(user);
-        navigate("/");
-        // Do something with the signed-in user, e.g. navigate to home page
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            const user = userCredential.user;
+            localStorage.setItem("access_token", user.accessToken);
+            console.log(user);
+            navigate("/");
+            // Do something with the signed-in user, e.g. navigate to home page
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setError(errorMessage);
+            setNotificationText(error.message);
+            setShowNotification(true);
+            setNotificationType("error");
+            setIsLoading(false);
+          });
       })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setError(errorMessage);
-        setNotificationText(error.message)
-        setShowNotification(true)
-        setNotificationType('error')
-        setIsLoading(false);
-      });
+      .catch(() => {});
   };
-
-
 
   const handleSignUp = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up successfully
-        const user = userCredential.user;
-        console.log(user);
+    form
+      .validateFields()
+      .then(() => {
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed up successfully
+            const user = userCredential.user;
+            localStorage.setItem("access_token", user.accessToken);
+            console.log(user);
+            navigate("/");
+          })
+          .catch((error) => {
+            setNotificationText(error.message);
+            setNotificationType("error");
+            setShowNotification(true);
+            // Handle sign-up errors here
+            console.log(error);
+          });
       })
-      .catch((error) => {
-        setNotificationText(error.message)
-        setNotificationType('error')
-        setShowNotification(true)
-        // Handle sign-up errors here
-        console.log(error);
-      });
+      .catch(() => {});
   };
-
 
   const apiData = {
     firebase_relay: firebase_relay_id,
@@ -141,40 +156,41 @@ const Login = () => {
     console.log(`switch to ${checked}`);
   };
   function handleSubmit() {
-    fetch('https://console.collect2play.com/api/auth/user_by_firebase_relay_id', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(apiData)
-    })
-      .then(response => {
+    fetch(
+      "https://console.collect2play.com/api/auth/user_by_firebase_relay_id",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(apiData),
+      }
+    )
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
-      .then(data => {
-        console.log(data)
+      .then((data) => {
+        console.log(data);
 
         // setIsLoading(false);
         // localStorage.setItem("access_token", data.id);
         // setData(data);
         // navigate("/");
       })
-      .catch(error => {
-        console.log(error)
+      .catch((error) => {
+        console.log(error);
         // setIsLoading(false);
         // setError(error);
       });
   }
 
-
   return (
     <>
       <div className="login_Background">
         <div className="container">
-
           <div className="row Wrapper align-items-center justify-content-center">
             <div className="col-md-6 px-3">
               <img
@@ -186,9 +202,13 @@ const Login = () => {
                 className="d-block m-auto"
               />
               <h1>{!isSignup ? "Register" : "Log in"}</h1>
-              <form>
+              <Form form={form}>
                 {showNotification && notificationText && (
-                  <Alert message={notificationText} type={notificationType} showIcon />
+                  <Alert
+                    message={notificationText}
+                    type={notificationType}
+                    showIcon
+                  />
                 )}
                 <motion.div
                   initial={{ opacity: 0, marginBottom: 0 }}
@@ -198,43 +218,100 @@ const Login = () => {
                   }}
                 >
                   {showNotification && notificationText && (
-                    <Alert message={notificationText} type={notificationType} showIcon />
+                    <Alert
+                      message={notificationText}
+                      type={notificationType}
+                      showIcon
+                    />
                   )}
                 </motion.div>
 
-                {hideshowemail && <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />}
-
-                {hideshowphone && (
-                  <input type="text" placeholder="Cell Phone" />
+                {hideshowemail && (
+                  <Form.Item
+                    name="email"
+                    type="email"
+                    rules={[
+                      {
+                        type: "email",
+                        message: "Please enter a valid Email Address",
+                      },
+                      {
+                        whitespace: true,
+                        required: true,
+                        message: "Please enter the value",
+                      },
+                    ]}
+                  >
+                    <Input
+                      placeholder="Email"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Form.Item>
                 )}
 
-                <Input.Password
-                  placeholder="Password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  iconRender={(visible) =>
-                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-                  }
-                />
-                {OTP && <input
-                  type="text"
-                  placeholder="OTP"
+                {hideshowphone && (
+                  <Form.Item
+                    name="phone-number"
+                    rules={[
+                      {
+                        whitespace: true,
+                        required: true,
+                        message: "Please enter the phone number",
+                      },
+                    ]}
+                  >
+                    <Input
+                      type="number"
+                      placeholder="Cell Phone"
+                      onChange={(e) => {
+                        setPhoneNumber(e.target.value);
+                      }}
+                    />
+                  </Form.Item>
+                )}
 
-                />}
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      whitespace: true,
+                      required: true,
+                      message: "Please enter the password",
+                    },
+                  ]}
+                >
+                  <Input.Password
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    iconRender={(visible) =>
+                      visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                    }
+                  />
+                </Form.Item>
+                {OTP && <input type="text" placeholder="OTP" />}
                 <div className="mt-3">
-                  <Switch onChange={onChange} className="mt-4"
-                    onClick={() => { handleClick(); handleOtherClick(); otpfield(); }}
+                  <Switch
+                    onChange={onChange}
+                    className="mt-4"
+                    onClick={() => {
+                      handleClick();
+                      handleOtherClick();
+                      otpfield();
+                    }}
                   />
                   <span className="float-end">Forgot Password?</span>
                 </div>
                 {/* <Link to="/home"> */}
                 <button
-                  onClick={() => { !isSignup ? handleSignUp() : handleSignIn() }}
-                  type="submit"
+                  onClick={() => {
+                    !isSignup ? handleSignUp() : handleSignIn();
+                  }}
+                  // type="submit"
                   className="signinbutton mt-4"
                 >
                   {!isSignup ? "Register" : "Log in"}
                 </button>
-              </form>
+              </Form>
               {/* </Link> */}
               <button type="submit" className="guest continue">
                 Continue as Guest
@@ -270,14 +347,23 @@ const Login = () => {
                 </div>
               </div>
               <div className="col-12 py-3 text-center">
-                <span>{isSignup ? "Don't have an account!" : "Already have an account!"} </span>
+                <span>
+                  {isSignup
+                    ? "Don't have an account!"
+                    : "Already have an account!"}{" "}
+                </span>
                 {/* <Link to="/sign_up"> */}
-                <span style={{ cursor: ' pointer' }} className="text-white ps-2" onClick={() => setIsSignup(!isSignup)}>{isSignup ? "Sign Up" : "Log in"}</span>
+                <span
+                  style={{ cursor: " pointer" }}
+                  className="text-white ps-2"
+                  onClick={() => setIsSignup(!isSignup)}
+                >
+                  {isSignup ? "Sign Up" : "Log in"}
+                </span>
                 {/* </Link> */}
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </>
@@ -285,6 +371,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
-
